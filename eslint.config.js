f6b6809f37c06ce071bee547de42295968e72a98
Ainsley <hello@ainsley.dev>
2025-03-17 14:00:02 +0000

    chore: Linting

--- eslint.config.js
+++ eslint.config.js
@@ -25,9 +25,9 @@ export const defaultESLintIgnores = [
 export default [
   ...payloadEsLintConfig,
   {
-    plugins: [
-      'perfectionist',
-    ],
+    plugins: {
+      perfectionist: require('eslint-plugin-perfectionist'),
+    },
     rules: {
       'no-restricted-exports': 'off',
       'semi': ['error', 'always'],
